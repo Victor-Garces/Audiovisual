88639872f841833cc0c7811b5e400430669c44ba
Victor <victorgarcesgg@gmail.com>
2019-04-01 19:34:31 -0400

    Fixed some bugs

--- WindowsFormsApp1/Forms/Empleado/EmpleadoLista.Designer.cs
+++ WindowsFormsApp1/Forms/Empleado/EmpleadoLista.Designer.cs
@@ -43,6 +43,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.empleadosBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.aUDIOVISUALDataSet)).BeginInit();
@@ -60,10 +61,11 @@
             this.fechaIngresoDataGridViewTextBoxColumn,
             this.estadoDataGridViewCheckBoxColumn});
             this.dataGridView1.DataSource = this.empleadosBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(84, 66);
+            this.dataGridView1.Location = new System.Drawing.Point(63, 54);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(656, 317);
+            this.dataGridView1.Size = new System.Drawing.Size(492, 258);
             this.dataGridView1.TabIndex = 0;
             // 
             // idDataGridViewTextBoxColumn
@@ -119,9 +121,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(86, 27);
+            this.button4.Location = new System.Drawing.Point(64, 22);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(119, 23);
+            this.button4.Size = new System.Drawing.Size(89, 19);
             this.button4.TabIndex = 44;
             this.button4.Text = "Volver a inicio";
             this.button4.UseVisualStyleBackColor = true;
@@ -129,9 +132,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(586, 27);
+            this.button1.Location = new System.Drawing.Point(443, 316);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(149, 23);
+            this.button1.Size = new System.Drawing.Size(112, 19);
             this.button1.TabIndex = 43;
             this.button1.Text = "Crear empleado";
             this.button1.UseVisualStyleBackColor = true;
@@ -140,30 +144,43 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(354, 27);
+            this.label1.Location = new System.Drawing.Point(264, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 17);
+            this.label1.Size = new System.Drawing.Size(73, 13);
             this.label1.TabIndex = 42;
             this.label1.Text = "EMPLEADOS";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(600, 389);
+            this.textBox1.Location = new System.Drawing.Point(449, 22);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(140, 22);
+            this.textBox1.Size = new System.Drawing.Size(106, 20);
             this.textBox1.TabIndex = 45;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(401, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 46;
+            this.label2.Text = "Buscar:";
+            // 
             // EmpleadoLista
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "EmpleadoLista";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "EmpleadoLista";
@@ -192,5 +209,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- WindowsFormsApp1/Forms/Usuario/UsuarioLista.Designer.cs
+++ WindowsFormsApp1/Forms/Usuario/UsuarioLista.Designer.cs
@@ -43,6 +43,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.usuariosBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.aUDIOVISUALDataSet1)).BeginInit();
@@ -60,10 +61,11 @@
             this.isActiveDataGridViewCheckBoxColumn,
             this.isAdminDataGridViewCheckBoxColumn});
             this.dataGridView1.DataSource = this.usuariosBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(74, 67);
+            this.dataGridView1.Location = new System.Drawing.Point(56, 54);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(649, 332);
+            this.dataGridView1.Size = new System.Drawing.Size(487, 270);
             this.dataGridView1.TabIndex = 0;
             // 
             // idDataGridViewTextBoxColumn
@@ -119,9 +121,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(574, 30);
+            this.button1.Location = new System.Drawing.Point(431, 328);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(149, 23);
+            this.button1.Size = new System.Drawing.Size(112, 19);
             this.button1.TabIndex = 4;
             this.button1.Text = "Crear usuario";
             this.button1.UseVisualStyleBackColor = true;
@@ -130,17 +133,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(342, 30);
+            this.label1.Location = new System.Drawing.Point(256, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.Size = new System.Drawing.Size(63, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "USUARIOS";
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(74, 30);
+            this.button4.Location = new System.Drawing.Point(56, 24);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(119, 23);
+            this.button4.Size = new System.Drawing.Size(89, 19);
             this.button4.TabIndex = 41;
             this.button4.Text = "Volver a inicio";
             this.button4.UseVisualStyleBackColor = true;
@@ -148,22 +153,34 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(590, 405);
+            this.textBox1.Location = new System.Drawing.Point(442, 30);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(133, 22);
+            this.textBox1.Size = new System.Drawing.Size(101, 20);
             this.textBox1.TabIndex = 42;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(394, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Buscar:";
+            // 
             // UsuarioLista
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "UsuarioLista";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "UsuarioLista";
@@ -192,5 +209,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
